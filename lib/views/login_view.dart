d349956e7b28e69661ed5bad7bb8c20c4cd7e02b
UVM <utkarshmore7588@gmail.com>
2022-09-03 01:24:58 +0530

    commit

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:notes/views/routes.dart';
 import 'dart:developer' as devtools show log;
 import '../firebase_options.dart';
+import '../utilities/show_error_dialog.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -67,10 +68,23 @@ class _LoginViewState extends State<LoginView> {
                 );
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'user-not-found') {
-                  devtools.log('User not found');
+                  await showErrorDialog(
+                    context,
+                    'User not found',
+                  );
                 } else if (e.code == 'wrong-password') {
-                  devtools.log('wrong password');
+                  await showErrorDialog(
+                    context,
+                    'Wrong credentials',
+                  );
+                } else {
+                  await showErrorDialog(context, 'Error:${e.code}');
                 }
+              } catch (e) {
+                await showErrorDialog(
+                  context,
+                  e.toString(),
+                );
               }
             },
             child: const Text('Login'),
